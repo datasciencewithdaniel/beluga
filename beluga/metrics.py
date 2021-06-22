f577354615eea5fb6bbcff7c9ef2a5b89ec24379
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-06-22 19:40:32 +0500

    Added headers to display for all functions

--- beluga/metrics.py
+++ beluga/metrics.py
@@ -21,7 +21,7 @@ def true_positive(predictions: Iterable[int], ground_truth: Iterable[int], raw=F
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'True Positive')
     return True
 
 
@@ -42,7 +42,7 @@ def true_negative(predictions: Iterable[int], ground_truth: Iterable[int], raw=F
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'True Negative')
     return True
 
 
@@ -63,7 +63,7 @@ def false_positive(predictions: Iterable[int], ground_truth: Iterable[int], raw=
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'False Positive')
     return True
 
 
@@ -84,7 +84,7 @@ def false_negative(predictions: Iterable[int], ground_truth: Iterable[int], raw=
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'False Negative')
     return True
 
 
@@ -103,7 +103,7 @@ def precision(predictions: Iterable[int], ground_truth: Iterable[int], raw=False
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'Precision')
     return True
 
 
@@ -122,7 +122,7 @@ def recall(predictions: Iterable[int], ground_truth: Iterable[int], raw=False):
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'Recall')
     return True
 
 
@@ -132,7 +132,7 @@ def sensitivity(predictions: Iterable[int], ground_truth: Iterable[int], raw=Fal
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'Sensitivity')
     return True
 
 
@@ -151,7 +151,7 @@ def specificity(predictions: Iterable[int], ground_truth: Iterable[int], raw=Fal
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'Specificity')
     return True
 
 
@@ -172,9 +172,11 @@ def f1(predictions: Iterable[int], ground_truth: Iterable[int], raw=False):
     if raw:
         return raw_metrics
 
-    helpers.display_helper(raw_metrics)
+    helpers.display_helper(raw_metrics, 'F1 score')
     return True
 
 
 if __name__ == "__main__":
     true_positive(np.array(["1", "1", "0"]), np.array(["1", "1", "0"]))
+    true_negative(np.array(["1", "1", "0"]), np.array(["1", "1", "0"]))
+    f1(np.array(["1", "1", "0"]), np.array(["1", "1", "0"]))
