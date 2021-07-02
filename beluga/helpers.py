0369dcbfecb68077de1eb00b2611dc9ab36c2396
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-07-02 18:00:32 +0500

    Check for empty arrays added in array_check method

--- beluga/helpers.py
+++ beluga/helpers.py
@@ -17,14 +17,6 @@ def display_helper(raw_metrics, header=""):
     return True
 
 
-# def error_check(metric_1, metric_2):  # POTENTIALLY REMOVE
-#     if metric_1 + metric_2 == 0:
-#         raise ZeroDivisionError("You tried to divide by zero, check your input data")
-#     if metric_1 + metric_2 < 0:
-#         raise ValueError("Your metric does not make sense, check your input data")
-#     return True
-
-
 def array_check(array_1, array_2):
     try:
         iter(array_1)
@@ -34,6 +26,9 @@ def array_check(array_1, array_2):
 
     if len(array_1) != len(array_2):
         raise ValueError("Your arrays do not match in length")
+
+    if len(array_1) == 0 or len(array_2) == 0:
+        raise ValueError("You passed empty arrays")
 
     if isinstance(array_1, np.ndarray) and isinstance(array_2, np.ndarray):
         return array_1, array_2
