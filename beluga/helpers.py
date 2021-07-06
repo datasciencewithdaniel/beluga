e06cf8e02cbd55e702e97826b750d6e6a57603fc
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-07-06 19:58:06 +0800

    docstrings for helpers.py

--- beluga/helpers.py
+++ beluga/helpers.py
@@ -3,6 +3,18 @@ import numpy as np
 
 
 def display_helper(raw_metrics: dict, header: str = "") -> bool:
+    """A helper function to print out the calculated metrics
+
+    Args:
+        raw_metrics (dict): The input metrics to print
+        header (str, optional): The title of the metrics. Defaults to "".
+
+    Raises:
+        ValueError: If the input metrics are not in Dictionary format
+
+    Returns:
+        bool: True if the print succeeds, False otherwise
+    """
 
     if not isinstance(raw_metrics, dict):
         raise ValueError("Your raw metrics is not a dictionary")
@@ -28,6 +40,20 @@ def display_helper(raw_metrics: dict, header: str = "") -> bool:
 def array_check(
     array_1: Collection, array_2: Collection
 ) -> Tuple[np.ndarray, np.ndarray]:
+    """A helper function to check if the given arrays are valid
+
+    Args:
+        array_1 (Collection): The first array to check
+        array_2 (Collection): The second array to check
+
+    Raises:
+        TypeError: If either of the given arrays are not iterable
+        ValueError: If the arrays are not of the same length
+        ValueError: If both of the arrays are empty
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Numpy arrays ready for future processing
+    """
 
     try:
         iter(array_1)
