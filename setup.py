9937e62aedf8a084bab9c8b70e2a948fec90439f
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-07-06 20:35:33 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="beluga",
     packages=find_packages(include=["beluga"]),
-    version="0.1.0",
+    version="1.0.0",
     description="A Python library to help make your Machine Learning easier",
     author="Data Science with Daniel",
     license="MIT",
